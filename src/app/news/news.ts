5deba649007257a224fec1a7d155a6fd2155a33b
mohitaSTechie <mohitashrivastava850@gmail.com>
2026-09-03 15:50:48 +0530

    implemented angular charts

--- src/app/news/news.ts
+++ src/app/news/news.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { AgCharts } from 'ag-charts-angular';
+import { AgChartOptions } from 'ag-charts-community';
 
 @Component({
-  imports: [],
+  imports: [AgCharts],
   selector: 'app-news',
   styleUrl: './news.scss',
   templateUrl: './news.html',
 })
-export class News {}
+export class News {
+  public chartOptions: AgChartOptions;
+
+  constructor() {
+    this.chartOptions = {
+      data: [
+        { month: 'Jan', avgTemp: 2.3, iceCreamSales: 162000 },
+        { month: 'Mar', avgTemp: 6.3, iceCreamSales: 302000 },
+        { month: 'May', avgTemp: 16.2, iceCreamSales: 800000 },
+        { month: 'Jul', avgTemp: 22.8, iceCreamSales: 1254000 },
+        { month: 'Sep', avgTemp: 14.5, iceCreamSales: 950000 },
+        { month: 'Nov', avgTemp: 8.9, iceCreamSales: 200000 },
+      ],
+
+      series: [{ type: 'bar', xKey: 'month', yKey: 'iceCreamSales' }],
+    };
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+import {
+  AllCommunityModule as AllChartsCommunityModule,
+  ModuleRegistry as ChartsModuleRegistry,
+} from 'ag-charts-community';
 
 ModuleRegistry.registerModules([AllCommunityModule]);
+ChartsModuleRegistry.registerModules([AllChartsCommunityModule]);
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
